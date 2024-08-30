f78e5b19e6b832cd1497ecb7c9295dbca2752f38
Audrey Aubouin <ca.haestie@gmail.com>
2024-08-30 13:47:34 +0200

    correctif bug reload dans url

--- src/Context.tsx
+++ src/Context.tsx
@@ -6,35 +6,67 @@ import Valeurs from './components/pages/Valeurs'
 import WelcomePage from './components/welcome_page/welcome'
 import {BASE_URL_SITE} from './config.tsx'
 import NotFound from './components/pages/NotFound'
-import { HashRouter, Routes, Route} from "react-router-dom";
+import { createBrowserRouter, Routes, Route, HashRouter} from "react-router-dom";
 
-function ThemContextProvider({}:{}) {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <WelcomePage />
+  },
+  {
+  path: "/valeurs",
+    element: <Valeurs />
+  },
+  {
+    path: "/presentation",
+    element: <Presentation />
+  },
+  {
+    path: "/actualites",
+    element: <Actualites />
+  },
+  {
+    path: "/adhesion",
+    element: <Adhesion />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  }
+ ,
+  {
+    path: "*",
+    element: <NotFound />
+  }
+])
+function ThemContextProvider({children}:{children: any}) {
 
   return (
     <div className="root">
-        <HashRouter basename={BASE_URL_SITE}>
-            <Routes>
-              <Route path="/" element={<WelcomePage />}>
-                Accueil
-              </Route>
-              <Route path="/valeurs" element={<Valeurs />}>
-                Présentation
-              </Route>
-              <Route path="/presentation" element={<Presentation />}>
-                Présentation
-              </Route>
-              <Route path="/actualites" element={<Actualites />}>
-                Actualités
-              </Route>
-              <Route path="/contact" element={<Contact />}>
-                Contact
-              </Route>
-              <Route path="/adhesion" element={<Adhesion />}>
-                Adhésion
-              </Route>
+      <HashRouter basename={BASE_URL_SITE} >
+   
+        <Routes>
+          <Route path="/" element={<WelcomePage />}>
+            Accueil
+          </Route>
+          <Route path="/valeurs" element={<Valeurs />}>
+            Présentation
+          </Route>
+          <Route path="/presentation" element={<Presentation />}>
+            Présentation
+          </Route>
+          <Route path="/actualites" element={<Actualites />}>
+            Actualités
+          </Route>
+          <Route path="/contact" element={<Contact />}>
+            Contact
+          </Route>
+          <Route path="/adhesion" element={<Adhesion />}>
+            Adhésion
+          </Route>
 
-            </Routes>
-
+      </Routes>
+       
       </HashRouter>
         
     
